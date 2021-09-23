8f79abdad6856464f526409650138e62ada1d77f
OH BUM JUN <beomjun0638@hanmail.net>
2021-09-23 15:45:47 +0900

    [Feat] Player Display in HomeScene

--- GameFramework/Include/Object/StageDoor.cpp
+++ GameFramework/Include/Object/StageDoor.cpp
@@ -1,4 +1,7 @@
 #include "StageDoor.h"
+#include "../Scene/SceneManager.h"
+#include "../Scene/HomeScene.h"
+#include "../Scene/MainScene.h"
 
 CStageDoor::CStageDoor()
 {
@@ -26,7 +29,7 @@ bool CStageDoor::Init()
 
 	SetPivot(0.5f, 0.5f);
 	CreateAnimation();
-	AddAnimation("StageDoor", true, 0.3f);
+	AddAnimation("StageDoor", true, 2.f);
 }
 
 void CStageDoor::Update(float DeltaTime)
@@ -52,4 +55,23 @@ void CStageDoor::Render(HDC hDC)
 CStageDoor* CStageDoor::Clone()
 {
 	return new CStageDoor(*this);
+}
+
+void CStageDoor::ChangeStage()
+{
+	switch (m_DoorStageType)
+	{
+	case EDoorStage_Type::Stage_Home:
+		CSceneManager::GetInst()->CreateScene<CHomeScene>();
+		break;
+	case EDoorStage_Type::Stage_One:
+		CSceneManager::GetInst()->CreateScene<CMainScene>();
+		break;
+	case EDoorStage_Type::Stage_Two:
+		// CSceneManager::GetInst()->CreateScene<CHomeScene>();
+		break;
+	case EDoorStage_Type::Stage_Three:
+		// CSceneManager::GetInst()->CreateScene<CHomeScene>();
+		break;
+	}
 }
--- GameFramework/Include/Scene/HomeScene.cpp
+++ GameFramework/Include/Scene/HomeScene.cpp
@@ -19,7 +19,6 @@ bool CHomeScene::Init()
 	// Animation
 	LoadAnimationSequence();
 	// Objects
-	/*
 	CPlayer* Player = CreateObject<CPlayer>("Player");
 	Player->SetCharacterInfo(60, 10, NORMAL_MONSTER_HP_MAX, NORMAL_MONSTER_MP_MAX,
 		1, 100, 100, 100, NORMAL_ATTACK_DISTANCE);
@@ -28,7 +27,6 @@ bool CHomeScene::Init()
 	GetCamera()->SetTarget(Player);
 	GetCamera()->SetTargetPivot(0.5f, 0.5f);
 
-	*/
 	Vector2 WorldResolution = m_Camera->GetWorldResolution();
 	CStageDoor* StageDoor_One = CreateObject<CStageDoor>("StageDoor", 
 		Vector2(300.f + rand() % 700, rand() % 100),
@@ -40,6 +38,188 @@ bool CHomeScene::Init()
 
 void CHomeScene::LoadAnimationSequence()
 {
+	// Player ---
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaRightIdle",
+		"LucidNunNaRightIdle", TEXT("Player/Right/astand.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaRightIdle",
+		255, 0, 255);
+
+	for (int i = 0; i < 6; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaRightIdle",
+			i * 82.f, 0.f, 82.f, 73.f);
+	}
+
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaRightWalk",
+		"LucidNunNaRightWalk", TEXT("Player/Right/awalk.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaRightWalk",
+		255, 0, 255);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaRightWalk",
+			i * 85.f, 0.f, 85.f, 75.f);
+	}
+
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaRightAttack",
+		"LucidNunNaRightAttack", TEXT("Player/Right/aswing.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaRightAttack",
+		255, 0, 255);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaRightAttack",
+			i * 176.f, 0.f, 176.f, 89.f);
+	}
+
+	// Slow Motion Skill
+	GetSceneResource()->CreateAnimationSequence("SkillSlowMotionAttack",
+		"SkillSlowMotionAttack", TEXT("Player/Right/ashoot1.bmp"));
+	GetSceneResource()->SetTextureColorKey("SkillSlowMotionAttack",
+		255, 0, 255);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("SkillSlowMotionAttack",
+			i * 70.f, 0.f, 70.f, 81.f);
+	}
+
+	// 텔레포트 캐릭터
+	/* 1번째 버전
+		GetSceneResource()->CreateAnimationSequence("LucidNunNaTeleport",
+			"LucidNunNaTeleport", TEXT("Player/teleport.bmp"));
+
+		GetSceneResource()->SetTextureColorKey("LucidNunNaTeleport",
+			255, 0, 255);
+
+		for (int i = 0; i < 10; ++i)
+		{
+			for (int j = 0; j < 10; ++j)
+			{
+				GetSceneResource()->AddAnimationFrameData("LucidNunNaTeleport",
+					j * 240.f, i * 320.f, 240.f, 320.f);
+			}
+		}
+	*/
+	// 3번째 버전
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaTeleport",
+		"LucidNunNaTeleport", TEXT("Player/teleport2.bmp"));
+
+	// 임시적으로 SetTexture 키 방지 ( 배경화면색도 하얀색 )
+	// GetSceneResource()->SetTextureColorKey("LucidNunNaTeleport",
+		// 255, 0, 255);
+
+	for (int i = 0; i < 11; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaTeleport",
+			i * 57.3f, 0.f, 57.3f, 78.f);
+	}
+
+
+	// 왼쪽 방향 기본 애니메이션 
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaLeftIdle",
+		"LucidNunNaLeftIdle", TEXT("Player/Left/astand_left.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaLeftIdle",
+		255, 0, 255);
+
+	for (int i = 0; i < 6; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaLeftIdle",
+			i * 82.f, 0.f, 82.f, 73.f);
+	}
+
+	// Left  Walk
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaLeftWalk",
+		"LucidNunNaLeftWalk", TEXT("Player/Left/awalk_left.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaLeftWalk",
+		255, 0, 255);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaLeftWalk",
+			i * 85.f, 0.f, 85.f, 75.f);
+	}
+
+	// Run
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaLeftRun",
+		"LucidNunNaLeftRun", TEXT("Player/Left/alert_left.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaLeftRun",
+		255, 0, 255);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaLeftRun",
+			i * 59.f, 0.f, 59.f, 87.f);
+	}
+
+	// right run
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaRightRun",
+		"LucidNunNaRightRun", TEXT("Player/Right/alert.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaRightRun",
+		255, 0, 255);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaRightRun",
+			i * 59.f, 0.f, 59.f, 87.f);
+	}
+
+	// Stun
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaStun",
+		"LucidNunNaStun", TEXT("Player.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaStun",
+		255, 0, 255);
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaStun",
+			i * 32.f, 0.f, 32.f, 32.f);
+	}
+
+	// Target Attack 
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaTargetAttack",
+		"LucidNunNaTargetAttack", TEXT("Player/TargetAttack.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaTargetAttack",
+		255, 0, 255);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaTargetAttack",
+			i * 192.f, 0.f, 192.f, 192.f);
+	}
+
+	// Destory
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaLeftDeath",
+		"LucidNunNaLeftDeath", TEXT("Player/Left/dead.bmp"));
+	GetSceneResource()->SetTextureColorKey("LucidNunNaLeftDeath",
+		255, 0, 255);
+	for (int i = 0; i < 1; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaLeftDeath",
+			i * 59.f, 0.f, 59.f, 68.f);
+	}
+
+	GetSceneResource()->CreateAnimationSequence("LucidNunNaRightDeath",
+		"LucidNunNaRightDeath", TEXT("Player/Right/Right_dead.bmp"));
+
+	GetSceneResource()->SetTextureColorKey("LucidNunNaRightDeath",
+		255, 0, 255);
+
+	for (int i = 0; i < 3; ++i)
+	{
+		GetSceneResource()->AddAnimationFrameData("LucidNunNaRightDeath",
+			i * 59.f, 0.f, 59.f, 68.f);
+	}
+
+	// Door ---
 	GetSceneResource()->CreateAnimationSequence("StageDoor",
 		"StageDoor", TEXT("DoorToNextStage.bmp"));
 	GetSceneResource()->SetTextureColorKey("StageDoor",
