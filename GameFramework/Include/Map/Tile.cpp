232b93312e32456f684214f75fd993d7a789e74a
OH BUM JUN <beomjun0638@hanmail.net>
2021-10-03 18:58:54 +0900

    [Feat] ScrollMap, TileMap both Rendered in MainScene.cpp

--- GameFramework/Include/Map/Tile.cpp
+++ GameFramework/Include/Map/Tile.cpp
@@ -1,13 +1,14 @@
+
 #include "Tile.h"
+#include "../Scene/SceneManager.h"
 #include "../Scene/Scene.h"
 #include "../Scene/Camera.h"
-#include "../Scene/SceneManager.h"
 #include "../GameManager.h"
 
 CTile::CTile() :
-	m_Index(0),
 	m_IndexX(0),
 	m_IndexY(0),
+	m_Index(0),
 	m_Option(ETileOption::Normal)
 {
 }
@@ -38,18 +39,16 @@ void CTile::Render(HDC hDC)
 	if (m_Texture)
 	{
 		CCamera* Camera = CSceneManager::GetInst()->GetScene()->GetCamera();
-		Vector2 CameraPos = Camera->GetPos();
 
-		// World 상 위치 x --> 현재 창 내의 위치 
-		m_Texture->Render(hDC,
-			m_Pos-CameraPos,
-			m_StartFrame,
-			m_Size);
+		Vector2	CameraPos = Camera->GetPos();
+
+		m_Texture->Render(hDC, m_Pos - CameraPos, m_StartFrame, m_Size);
 	}
 
 	if (CGameManager::GetInst()->IsEditorMode())
 	{
-		HBRUSH Brush = 0;
+		HBRUSH	Brush = 0;
+
 		switch (m_Option)
 		{
 		case ETileOption::Normal:
@@ -61,19 +60,20 @@ void CTile::Render(HDC hDC)
 		case ETileOption::Slow:
 			Brush = CGameManager::GetInst()->GetYellowBrush();
 			break;
-	}
+		}
 
 		CCamera* Camera = CSceneManager::GetInst()->GetScene()->GetCamera();
-		Vector2 CameraPos = Camera->GetPos();
 
-		RECT rc;
-		rc.left   = (LONG)m_Pos.x - (LONG)CameraPos.x;
-		rc.top    = (LONG)m_Pos.y - (LONG)CameraPos.y;
-		rc.right  = (LONG)m_Pos.x + (LONG)m_Size.x - (LONG)CameraPos.x;
+		Vector2	CameraPos = Camera->GetPos();
+
+		RECT	rc;
+		rc.left = (LONG)m_Pos.x - (LONG)CameraPos.x;
+		rc.top = (LONG)m_Pos.y - (LONG)CameraPos.y;
+		rc.right = (LONG)m_Pos.x + (LONG)m_Size.x - (LONG)CameraPos.x;
 		rc.bottom = (LONG)m_Pos.y + (LONG)m_Size.y - (LONG)CameraPos.y;
 
-		FrameRect(hDC,&rc,Brush);
 
+		FrameRect(hDC, &rc, Brush);
 	}
 }
 
@@ -92,14 +92,15 @@ void CTile::Save(FILE* pFile)
 
 	if (m_Texture)
 	{
-		// Texture 존재 여부를 저장한다 
-		bool Tex = true;
+		bool	Tex = true;
 		fwrite(&Tex, sizeof(bool), 1, pFile);
+
 		m_Texture->Save(pFile);
 	}
+
 	else
 	{
-		bool Tex = false;
+		bool	Tex = false;
 		fwrite(&Tex, sizeof(bool), 1, pFile);
 	}
 }
@@ -117,11 +118,11 @@ void CTile::Load(FILE* pFile)
 	fread(&m_EndFrame, sizeof(Vector2), 1, pFile);
 	fread(&m_Option, sizeof(ETileOption), 1, pFile);
 
-	bool Tex = true;
+	bool	Tex = true;
 	fread(&Tex, sizeof(bool), 1, pFile);
+
 	if (Tex)
 	{
 		m_Texture = CTexture::LoadStatic(pFile, m_Scene);
 	}
-
 }
