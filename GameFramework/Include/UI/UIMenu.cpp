1ec326ecf7bd3cf6ad53888e9140f67a0d284581
OH BUM JUN <beomjun0638@hanmail.net>
2021-10-21 21:32:49 +0900

    [Feat] Start, Edit, Setting, Exit Btn pos set in Menu Scene

--- GameFramework/Include/UI/UIMenu.cpp
+++ GameFramework/Include/UI/UIMenu.cpp
@@ -27,55 +27,58 @@ bool CUIMenu::Init()
 	Back = CreateWidget<CUIImage>("Back");
 	Back->SetTexture("Logo", TEXT("images/MenuScene/small_logoRHS.bmp"));
 	Back->SetTextureColorKey(255, 255, 255);
-	Back->SetPos(100.f, 200.f);
+	Back->SetPos(0.f, 75.f);
+	Back->SetZOrder(1);
 
 	// Btn 1
 	CButton* Button = CreateWidget<CButton>("StartButton");
 	Button->SetTexture("StartButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
 	Button->SetTextureColorKey(255, 255, 255);
-	Button->SetPos(100.f, RS.Height / 2.f - 175.f);
-	Button->SetFrameData(EButton_State::Normal, Vector2(40.f, 0.f), Vector2(332.f, 41.f));
-	Button->SetFrameData(EButton_State::MouseOn, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Click, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Disable, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
+	Button->SetPos(0.f, RS.Height / 2.f);
+	Button->SetFrameData(EButton_State::Normal, Vector2(100.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::MouseOn, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Click, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Disable, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
 	Button->SetMouseOnSound("ButtonMouseOn");
 	Button->SetClickSound("ButtonClick");
 	Button->SetZOrder(1);
 	// Button->SetClickCallback<CUIStart>(this, &CUIStart::StartClick);
 
 	// Btn 2
-	Button->SetTexture("StartButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
+	Button = CreateWidget<CButton>("EditorButton");
+	Button->SetTexture("EditorButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
 	Button->SetTextureColorKey(255, 255, 255);
-	Button->SetPos(100.f, RS.Height / 2.f - 175.f);
-	Button->SetFrameData(EButton_State::Normal, Vector2(40.f, 0.f), Vector2(332.f, 41.f));
-	Button->SetFrameData(EButton_State::MouseOn, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Click, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Disable, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
+	Button->SetPos(-50.f, RS.Height / 2.f + 75.f);
+	Button->SetFrameData(EButton_State::Normal, Vector2(100.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::MouseOn, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Click, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Disable, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
 	Button->SetMouseOnSound("ButtonMouseOn");
 	Button->SetClickSound("ButtonClick");
 	Button->SetZOrder(1);
 
 	// Btn 3
-
-	Button->SetTexture("StartButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
+	Button = CreateWidget<CButton>("SettingButton");
+	Button->SetTexture("SettingButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
 	Button->SetTextureColorKey(255, 255, 255);
-	Button->SetPos(100.f, RS.Height / 2.f - 175.f);
-	Button->SetFrameData(EButton_State::Normal, Vector2(40.f, 0.f), Vector2(332.f, 41.f));
-	Button->SetFrameData(EButton_State::MouseOn, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Click, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Disable, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
+	Button->SetPos(-100.f, RS.Height / 2.f + 150.f);
+	Button->SetFrameData(EButton_State::Normal, Vector2(100.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::MouseOn, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Click, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Disable, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
 	Button->SetMouseOnSound("ButtonMouseOn");
 	Button->SetClickSound("ButtonClick");
 	Button->SetZOrder(1);
 
 	// Btn 4
-	Button->SetTexture("StartButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
+	Button = CreateWidget<CButton>("ExitButton");
+	Button->SetTexture("ExitButton", TEXT("images/MenuScene/small_MenuBtn.bmp"));
 	Button->SetTextureColorKey(255, 255, 255);
-	Button->SetPos(100.f, RS.Height / 2.f - 175.f);
-	Button->SetFrameData(EButton_State::Normal, Vector2(40.f, 0.f), Vector2(332.f, 41.f));
-	Button->SetFrameData(EButton_State::MouseOn, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Click, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
-	Button->SetFrameData(EButton_State::Disable, Vector2(371.f, 0.f), Vector2(372.f, 41.f));
+	Button->SetPos(-150.f, RS.Height / 2.f + 225.f);
+	Button->SetFrameData(EButton_State::Normal, Vector2(100.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::MouseOn, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Click, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
+	Button->SetFrameData(EButton_State::Disable, Vector2(680.f, 15.f), Vector2(470.f, 36.f));
 	Button->SetMouseOnSound("ButtonMouseOn");
 	Button->SetClickSound("ButtonClick");
 	Button->SetZOrder(1);
