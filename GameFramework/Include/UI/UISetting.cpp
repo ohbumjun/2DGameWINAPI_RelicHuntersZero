5ae1a44ac7f4be268dbd18024c9fb07440752d8c
OH BUM JUN <beomjun0638@hanmail.net>
2021-10-22 10:47:05 +0900

    [Feat] Volume Text, Btn Imge, Pos Set in Menu Scene

--- GameFramework/Include/UI/UISetting.cpp
+++ GameFramework/Include/UI/UISetting.cpp
@@ -5,6 +5,7 @@
 #include "../Scene/StartScene.h"
 #include "../Scene/EditorScene.h"
 #include "UIText.h"
+#include "Button.h"
 #include "UIImage.h"
 
 CUISetting::CUISetting() : 
@@ -17,13 +18,9 @@ CUISetting::~CUISetting()
 {
 }
 
-bool CUISetting::Init()
+void CUISetting::SetTextImages()
 {
 	Resolution	RS = CGameManager::GetInst()->GetResolution();
-
-	CUIImage* Back = CreateWidget<CUIImage>("Back");
-	Back->SetTexture("MenuBack", TEXT("images/MenuScene/bg_menu_settings.bmp"));
-
 	// Left Top
 	m_LTBtn = CreateWidget<CUIImage>("LTTitle");
 	m_LTBtn->SetTexture("LTTitle", TEXT("images/MenuScene/spr_menu_button_1.bmp"));
@@ -41,7 +38,7 @@ bool CUISetting::Init()
 	m_RTBtn = CreateWidget<CUIImage>("RTTitle");
 	m_RTBtn->SetTexture("RTTitle", TEXT("images/MenuScene/spr_menu_button_1.bmp"));
 	m_RTBtn->SetTextureColorKey(255, 255, 255);
-	m_RTBtn->SetPos(590.f, RS.Height / 2.f - 75.f );
+	m_RTBtn->SetPos(590.f, RS.Height / 2.f - 75.f);
 	m_RTBtn->SetZOrder(1);
 
 	m_RTText = CreateWidget<CUIText>("RTText");
@@ -49,7 +46,137 @@ bool CUISetting::Init()
 	m_RTText->SetPos(630.f, RS.Height / 2.f - 75.f);
 	m_RTText->SetText(TEXT("MUSIC OPTIONS"));
 	m_RTText->SetZOrder(2);
+}
 
+void CUISetting::SetMainVolumeElements()
+{
+	// Text
+	m_MVText = CreateWidget<CUIText>("MVText");
+	m_MVText->SetTextColor(255, 0, 0);
+	m_MVText->SetPos(30.f, 175.f);
+	m_MVText->SetText(TEXT("Master Volume"));
+	m_MVText->SetZOrder(2);
+
+	m_MVImage = CreateWidget<CUIImage>("MVImg");
+	m_MVImage->SetTexture("MVImg", TEXT("images/MenuScene/spr_menu_img.bmp"));
+	m_MVImage->SetPos(300.f, 175.f);
+	m_MVImage->SetZOrder(2);
+
+	m_MVLeftBtn= CreateWidget<CButton>("MVLeftBtn");
+	m_MVLeftBtn->SetTexture("MVLeftBtn", TEXT("images/MenuScene/spr_menu_minus_0.bmp"));
+	m_MVLeftBtn->SetTextureColorKey(255, 255, 255);
+	m_MVLeftBtn->SetPos(270.f, 175.f);
+	m_MVLeftBtn->SetFrameData(EButton_State::Normal, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVLeftBtn->SetFrameData(EButton_State::MouseOn, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVLeftBtn->SetFrameData(EButton_State::Click, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVLeftBtn->SetFrameData(EButton_State::Disable, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVLeftBtn->SetMouseOnSound("ButtonMouseOn");
+	m_MVLeftBtn->SetClickSound("ButtonClick");
+	m_MVLeftBtn->SetZOrder(1);
+
+	// class CButton* m_MVRightBtn;
+	m_MVRightBtn = CreateWidget<CButton>("MVRightBtn");
+	m_MVRightBtn->SetTexture("MVRightBtn", TEXT("images/MenuScene/spr_menu_plus_0.bmp"));
+	m_MVRightBtn->SetTextureColorKey(255, 255, 255);
+	m_MVRightBtn->SetPos(367.5f, 175.f);
+	m_MVRightBtn->SetFrameData(EButton_State::Normal, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVRightBtn->SetFrameData(EButton_State::MouseOn, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVRightBtn->SetFrameData(EButton_State::Click, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVRightBtn->SetFrameData(EButton_State::Disable, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_MVRightBtn->SetMouseOnSound("ButtonMouseOn");
+	m_MVRightBtn->SetClickSound("ButtonClick");
+	m_MVRightBtn->SetZOrder(1);
+}
+
+void CUISetting::SetBackgroundVolumeElements()
+{
+	m_BVText = CreateWidget<CUIText>("BVText");
+	m_BVText->SetTextColor(255, 0, 0);
+	m_BVText->SetPos(30.f, 225.f);
+	m_BVText->SetText(TEXT("Background Volume"));
+	m_BVText->SetZOrder(2);
+
+	m_BVImage = CreateWidget<CUIImage>("BVImg");
+	m_BVImage->SetTexture("BVImg", TEXT("images/MenuScene/spr_menu_img.bmp"));
+	m_BVImage->SetPos(300.f, 225.f);
+	m_BVImage->SetZOrder(2);
+
+	m_BVLeftBtn = CreateWidget<CButton>("BVLeftBtn");
+	m_BVLeftBtn->SetTexture("BVLeftBtn", TEXT("images/MenuScene/spr_menu_minus_0.bmp"));
+	m_BVLeftBtn->SetTextureColorKey(255, 255, 255);
+	m_BVLeftBtn->SetPos(270.f, 225.f);
+	m_BVLeftBtn->SetFrameData(EButton_State::Normal, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVLeftBtn->SetFrameData(EButton_State::MouseOn, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVLeftBtn->SetFrameData(EButton_State::Click, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVLeftBtn->SetFrameData(EButton_State::Disable, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVLeftBtn->SetMouseOnSound("ButtonMouseOn");
+	m_BVLeftBtn->SetClickSound("ButtonClick");
+	m_BVLeftBtn->SetZOrder(1);
+
+	// class CButton* m_MVRightBtn;
+	m_BVRightBtn = CreateWidget<CButton>("BVRightBtn");
+	m_BVRightBtn->SetTexture("BVRightBtn", TEXT("images/MenuScene/spr_menu_plus_0.bmp"));
+	m_BVRightBtn->SetTextureColorKey(255, 255, 255);
+	m_BVRightBtn->SetPos(367.5f, 225.f);
+	m_BVRightBtn->SetFrameData(EButton_State::Normal, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVRightBtn->SetFrameData(EButton_State::MouseOn, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVRightBtn->SetFrameData(EButton_State::Click, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVRightBtn->SetFrameData(EButton_State::Disable, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_BVRightBtn->SetMouseOnSound("ButtonMouseOn");
+	m_BVRightBtn->SetClickSound("ButtonClick");
+	m_BVRightBtn->SetZOrder(1);
+}
+
+void CUISetting::SetEffectVolumeElements()
+{
+	m_EVText = CreateWidget<CUIText>("EVText");
+	m_EVText->SetTextColor(255, 0, 0);
+	m_EVText->SetPos(30.f, 275.f);
+	m_EVText->SetText(TEXT("Effect Volume"));
+	m_EVText->SetZOrder(2);
+
+	m_EVImage = CreateWidget<CUIImage>("EVImg");
+	m_EVImage->SetTexture("EVImg", TEXT("images/MenuScene/spr_menu_img.bmp"));
+	m_EVImage->SetPos(300.f, 275.f);
+	m_EVImage->SetZOrder(2);
+
+	m_EVLeftBtn = CreateWidget<CButton>("EVLeftBtn");
+	m_EVLeftBtn->SetTexture("EVLeftBtn", TEXT("images/MenuScene/spr_menu_minus_0.bmp"));
+	m_EVLeftBtn->SetTextureColorKey(255, 255, 255);
+	m_EVLeftBtn->SetPos(270.f, 275.f);
+	m_EVLeftBtn->SetFrameData(EButton_State::Normal, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVLeftBtn->SetFrameData(EButton_State::MouseOn, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVLeftBtn->SetFrameData(EButton_State::Click, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVLeftBtn->SetFrameData(EButton_State::Disable, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVLeftBtn->SetMouseOnSound("ButtonMouseOn");
+	m_EVLeftBtn->SetClickSound("ButtonClick");
+	m_EVLeftBtn->SetZOrder(1);
+
+	m_EVRightBtn = CreateWidget<CButton>("EVRightBtn");
+	m_EVRightBtn->SetTexture("EVRightBtn", TEXT("images/MenuScene/spr_menu_plus_0.bmp"));
+	m_EVRightBtn->SetTextureColorKey(255, 255, 255);
+	m_EVRightBtn->SetPos(367.5f, 275.f);
+	m_EVRightBtn->SetFrameData(EButton_State::Normal, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVRightBtn->SetFrameData(EButton_State::MouseOn, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVRightBtn->SetFrameData(EButton_State::Click, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVRightBtn->SetFrameData(EButton_State::Disable, Vector2(0.f, 0.f), Vector2(13.f, 26.f));
+	m_EVRightBtn->SetMouseOnSound("ButtonMouseOn");
+	m_EVRightBtn->SetClickSound("ButtonClick");
+	m_EVRightBtn->SetZOrder(1);
+}
+
+bool CUISetting::Init()
+{
+	Resolution	RS = CGameManager::GetInst()->GetResolution();
+
+	CUIImage* Back = CreateWidget<CUIImage>("Back");
+	Back->SetTexture("MenuBack", TEXT("images/MenuScene/bg_menu_settings.bmp"));
+
+	SetTextImages();
+	SetMainVolumeElements();
+	SetBackgroundVolumeElements();
+	SetEffectVolumeElements();
+	
 	return true;
 }
 
--- GameFramework/Include/UI/UISetting.h
+++ GameFramework/Include/UI/UISetting.h
@@ -11,11 +11,31 @@ private:
 // Btns 
 private :
 	class CUIImage* m_LTBtn;
+	class CUIText* m_LTText;
 	class CUIImage* m_RTBtn;
+	class CUIText* m_RTText;
+	void  SetTextImages();
 // Texts
 private :
-	class CUIText* m_LTText;
-	class CUIText* m_RTText;
+// Volume
+private :
+	class CUIText*  m_MVText;
+	class CUIImage* m_MVImage;
+	class CButton*  m_MVLeftBtn;
+	class CButton*  m_MVRightBtn;
+	void SetMainVolumeElements();
+private :
+	class CUIText*  m_BVText;
+	class CUIImage* m_BVImage;
+	class CButton*  m_BVLeftBtn;
+	class CButton*  m_BVRightBtn;
+	void SetBackgroundVolumeElements();
+private :
+	class CUIText*  m_EVText;
+	class CUIImage* m_EVImage;
+	class CButton*  m_EVLeftBtn;
+	class CButton*  m_EVRightBtn;
+	void SetEffectVolumeElements();
 public:
 	virtual bool Init();
 	virtual void Update(float DeltaTime);
